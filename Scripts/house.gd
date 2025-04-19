505095dfdcbdb8cb03bca79fcb1d396940091f55
seollemi <kc.pingol6104@gmail.com>
2025-04-19 16:14:57 +0800

    some works
    minor works and fixes

--- Scripts/house.gd
+++ Scripts/house.gd
@@ -1,17 +1,29 @@
 extends Node2D
 
 func _ready() -> void:
-	Dialogic.start("timeline")
 	if Global.game_first_loadin == true:
 		$Player.position.x = Global.player_start_posx
 		$Player.position.y = Global.player_start_posy
+		start_dialog()
 	else: 
 		$Player.position.x = Global.player_enter_house_posx
 		$Player.position.y = Global.player_enter_house_posy
 	
+		
 func _process(delta: float) -> void:
 	change_scene()
 
+
+func start_dialog():
+	Dialogic.timeline_ended.connect(_on_timeline_ended)
+	Dialogic.start("timeline")
+	$Door_outside/CollisionShape2D.disabled = true
+	
+	
+
+func _on_timeline_ended():
+	Dialogic.timeline_ended.disconnect(_on_timeline_ended)
+	$Door_outside/CollisionShape2D.disabled = false
 
 
 func _on_door_outside_body_entered(body: Node2D) -> void:
