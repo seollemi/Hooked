e4cd62808fdb53623fc1b363627ad01e5305b472
seollemi <kc.pingol6104@gmail.com>
2025-04-25 22:29:20 +0800

    fixed office(confict)

--- Scripts/office.gd
+++ Scripts/office.gd
@@ -5,7 +5,7 @@ extends Node2D
 
 func _ready() -> void:
 	interactable.interact = _on_interact
-	$Marker/AnimationPlayer.play("marker")
+	#$Marker/AnimationPlayer.play("marker")
 func _process(delta: float) -> void:
 	change_scene()
 
@@ -15,7 +15,15 @@ func _on_interact():
 	
 func change_scene():
 	if Global.transition_scene:
-		if Global.next_scene == "computer":
-			ChangeScene.change_scene_anim("res://Scenes/outsidehouse.tscn")
-			Global.game_first_loadin = false
-			Global.finish_changescenes()
+		match Global.next_scene:
+			"outside":
+				ChangeScene.change_scene_anim("res://Scenes/bridge.tscn")
+			"officelobby":
+				ChangeScene.change_scene_anim("res://Scenes/officelobby.tscn")
+		Global.finish_changescenes()
+
+
+func _on_door_to_office_body_entered(body: Node2D) -> void:
+	if body is Player:
+		Global.next_scene = "officelobby"
+		Global.transition_scene = true
