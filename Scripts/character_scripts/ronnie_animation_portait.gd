d42f3e03d115b8ea414bbd741cde65933abb9fd4
linglinglover <viloriajameserrol@gmail.com>
2025-04-28 14:12:44 +0800

    fixes/ minor works

--- Scripts/character_scripts/ronnie_animation_portait.gd
+++ Scripts/character_scripts/ronnie_animation_portait.gd
@@ -11,26 +11,23 @@ func _update_portrait(passed_character: DialogicCharacter, passed_portrait: Stri
 		passed_portrait = passed_character['default_portrait']
 	
 	if passed_portrait == "ronnie_talking":
-		if $AnimatedSprite2D.sprite_frames.has_animation("ronnie_talking"):
+		if %AnimationPlayer.has_animation("ronnie_talking"):
 			print("Playing animation:", "ronnie_talking")
-			$AnimatedSprite2D.play("ronnie_talking")
+			%AnimationPlayer.play("ronnie_talking")
 	elif passed_portrait == "ronnie_no":	
-		if $AnimatedSprite2D.sprite_frames.has_animation("ronnie_no"):
+		if %AnimationPlayer.has_animation("ronnie_no"):
 			print("Playing animation:", "ronnie_no")
-			$AnimatedSprite2D.play("ronnie_no")
+			%AnimationPlayer.play("ronnie_no")
 
 		
 
 func _on_animation_player_animation_finished(anim_name: String) -> void:
-	if $AnimatedSprite2D.has_animation(anim_name):
-		$AnimatedSprite2D.play(anim_name)
+	if %AnimationPlayer.has_animation(anim_name):
+		%AnimationPlayer.play(anim_name)
 
 func _get_covered_rect() -> Rect2:
-	var anim_sprite = $AnimatedSprite2D
-	var current_anim = anim_sprite.animation
-	var frame_idx = anim_sprite.frame
-	var texture = anim_sprite.sprite_frames.get_frame_texture(current_anim, frame_idx)
-
+	var sprite_node = %ronnie  # Make sure your scene has a Sprite2D node for this to work
+	var texture = sprite_node.texture
 	if texture:
-		return Rect2(anim_sprite.position, texture.get_size() * anim_sprite.scale)
+		return Rect2(sprite_node.position, texture.get_size() * sprite_node.scale)
 	return Rect2()
