d6eb0194981358e824ca25ffc32ec4c13c146032
leFlamee <164747969+leFlamee@users.noreply.github.com>
2025-04-29 02:52:49 +0800

    update global

--- Scripts/Global/Global.gd
+++ Scripts/Global/Global.gd
@@ -28,7 +28,7 @@ var player_bridgecutscene_posx = 497.0
 var player_bridgecutscene_posy = -248.0
 #PLAYER EVENT MOVEMENT
 var player_PC_Location = Vector2(463.0, 76.0)
-var teleport_back := false  
+var teleport_back := false
 var dialog_pc_opened := false # ano to it only happen 1 time permanently even across scene reloads
 var pc_start_opened: bool = false  # interact once only happen 1 time permanently even across scene reloads
 var mail_open_opened: bool = false  # interact once only happen 1 time permanently even across scene reloads 
@@ -36,18 +36,19 @@ var fakelogin_open_opened: bool = false
 var gate_cutscene_done: bool = false
 var bridge_cutscene_done: bool = false
 var act_1_done: bool = false
+
  # Start at zero
 var global_triggered = false
 var global_alex = false
 
 #MINI GAME_Password_Global
+var collected_hint_ids: Array = []
 var collected_hints: int = 0 # Start at zero
 var hints_read: int = 0  # counts how many hints have been opened
 var scrolling_background: Node2D = null
 var player_points: int = 0
 var chosen_password_number: int = 1  # default 1 = Andrew
 
-#Fade one tiem
 var has_faded_in: bool = false
 
 func add_points(points_to_add: int) -> void:
@@ -55,8 +56,7 @@ func add_points(points_to_add: int) -> void:
 	print("⭐ Points Added: ", points_to_add, " | Total Points: ", player_points)
 
 func reset_points_only() -> void:
-	player_points = 0  
-
+	player_points = 0
 #NPC MOVEMENT
 var NPC_Office_location = Vector2(463.0, 76.0)
 
