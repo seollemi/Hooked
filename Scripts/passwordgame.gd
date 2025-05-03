e5a8dff7b19bc152c9381b8f18d582f6476cc7cb
leFlamee <164747969+leFlamee@users.noreply.github.com>
2025-05-03 17:22:37 +0800

    scrambled instruction

--- Scripts/passwordgame.gd
+++ Scripts/passwordgame.gd
@@ -11,4 +11,4 @@ func _process(delta: float) -> void:
 
 
 func _on_TextureButton_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/emailcomp.tscn")
+	get_tree().change_scene_to_file("res://Scenes/password_instruction.tscn")
